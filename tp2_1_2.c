a336783e6f155801c7362db4e7fb76466d6ede15
Lucia Alderete <lu.4ld3r3t3@gmail.com>
2026-04-10 16:59:09 -0300

    ejercicio 3 completado

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -7,12 +7,13 @@ int main()
 {
     int i;
     double vt[N];
+    double *punt = vt;
     
     srand(time(NULL));
     for (i = 0; i < N; i++)
     {
-        *(vt + i)= 1 + rand() % 100;
-        printf("\n%.2f", *(vt + i));
+        *(punt + i)= 1 + rand() % 100;
+        printf("\n%.2f", *(punt + i));
     }
       
 }
